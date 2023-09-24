49ed4736d1b4596626a06fada623b4f7084866ea
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-09-23 23:24:16 -0600

    Render single TopicListItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,22 @@ import React from 'react';
 import PhotoList from 'components/PhotoList';
 //import PhotoFavButton from 'components/PhotoFavButton';
 import './App.scss';
+import TopicListItem from 'components/TopicListItem';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const sampleDataForTopicListItem = {
+    id: "1",
+    slug: "topic-1",
+    label: "Nature",
+  };
   return (
     <div className="App">
+      <TopicListItem data={sampleDataForTopicListItem}/>
       {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
       <PhotoList />
+
     </div>
   );
 };
