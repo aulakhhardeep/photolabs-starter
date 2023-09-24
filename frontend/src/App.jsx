3cb4dd46effca2fbeff799a5e3daace89447a244
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-09-23 22:50:14 -0600

    Build PhotoList component by rendering multiple Photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,17 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
-import PhotoFavButton from 'components/PhotoFavButton';
+//import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
+//import PhotoFavButton from 'components/PhotoFavButton';
 import './App.scss';
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  
-  const sampleDataForPhotoListItem = {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  };
-  
-  const photoComponents = [];
-  for (let i = 0; i < 3; i++) {
-    photoComponents.push(
-    <div key={i}>
-      <PhotoFavButton />
-      <PhotoListItem data={sampleDataForPhotoListItem} />
-      
-    </div>
-  );
-}
-
-
   return (
     <div className="App">
-      {photoComponents}
+      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+      <PhotoList />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
-
-
-
 const PhotoListItem = (props) => {
- const { data } = props;
- return (
-  <div className="photo-list__item">
-    <img src={data.imageSource} alt="Sample" className="photo-list__image" />
-    <img src={data.profile} alt="profile-pic" className="photo-list__user-profile" />
-    <h2 className="photo-list__user-info">{data.username}</h2>
-    <p className="photo-list__user-location">{`${data.location.city}, ${data.location.country}`}</p>
-  </div>
-
- );
-  
+  const { data } = props;
+  return (
+    <div className="photo-list__item">
+      <img src={data.urls.regular} alt="Sample" className="photo-list__image" />
+      <img src={data.user.profile} alt="profile-pic" className="photo-list__user-profile" />
+      <h2 className="photo-list__user-info">{data.user.username}</h2>
+      <p className="photo-list__user-location">{`${data.location.city}, ${data.location.country}`}</p>
+    </div>
+  );
 };
 
 export default PhotoListItem;
